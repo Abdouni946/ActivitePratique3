5d8760c8560b1ad8854dff23e84acfd103376c98
Zakariae ABDOUNI <abdouni.zakariae@gmail.com>
2024-03-11 03:06:40 +0000

    Ui Update

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -1,13 +1,14 @@
 package ma.enset.hopital.web;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.ui.Model;
 import lombok.AllArgsConstructor;
 import ma.enset.hopital.entities.patient;
 import ma.enset.hopital.repository.PatientRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
@@ -17,10 +18,12 @@ public class PatientController {
 
     @GetMapping("/index")
 
-    public String index( Model model){
-        List<patient> patientList = patientRepository.findAll();
-        model.addAttribute("listPatients", patientList);
-
+    public String index(Model model, @RequestParam(name = "page" , defaultValue = "0" ) int page,
+                                     @RequestParam(name = "size" , defaultValue = "4" ) int size){
+        Page<patient> patientPage = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("listPatients", patientPage.getContent());
+        model.addAttribute("pages", new int[patientPage.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "patient";
     }
 
